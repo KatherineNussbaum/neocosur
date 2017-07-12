5541564dd62a331e78b47e5dd7f8ed0bf1171ea7
KatherineNussbaum <knussbaum@gmail.com>
2017-07-12 00:31:30 -0400

    2017-07-12_Centros_Avance
    Avance Centros 2

---
+++ admin/centros/recursos.php
@@ -0,0 +1,68 @@
+<div class="ficha panel panel-default">
+  <div class="panel-body">
+  <form>
+    <h4> <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span> Recursos</h4>
+
+    <div class="col-lg-6">
+    
+      <div class="form-group row">
+        <label for="ventilacion" class="col-lg-6 control-label">Ventilación Alta frecuencia</label>
+        <label for="ventilacion" class="control-label radio-inline col-lg-2">
+          <input type="radio" name="ventilacion" value="si"> Sí
+        </label>
+        <label for="ventilacion" class="control-label radio-inline col-lg-2" >
+          <input type="radio" name="ventilacion" value="no"> No
+        </label>
+        <label for="ventilacion" class="control-label radio-inline col-lg-1" >
+          <input type="radio" name="ventilacion" value="s_i"> S/I
+        </label>
+      </div>
+
+      <div class="form-group row">
+        <label for="oxido" class="col-lg-6 control-label">Oxido Nítrico</label>
+          <label for="oxido" class="control-label radio-inline col-lg-2">
+            <input type="radio" name="oxido" value="si"> Sí
+          </label>
+          <label for="oxido" class="control-label radio-inline col-lg-2" >
+            <input type="radio" name="oxido" value="no"> No
+          </label>
+          <label for="oxido" class="control-label radio-inline col-lg-1" >
+            <input type="radio" name="oxido" value="s_i"> S/I
+          </label>
+      </div>
+
+      <div class="form-group row">
+        <label for="cirugia_general" class="col-lg-6 control-label">Cirugía General</label>
+          <label for="cirugia_general" class="control-label radio-inline col-lg-2">
+            <input type="radio" name="cirugia_general" value="si"> Sí
+          </label>
+          <label for="cirugia_general" class="control-label radio-inline col-lg-2" >
+            <input type="radio" name="cirugia_general" value="no"> No
+          </label>
+          <label for="cirugia_general" class="control-label radio-inline col-lg-1" >
+            <input type="radio" name="cirugia_general" value="s_i"> S/I
+          </label>
+      </div>
+
+      <div class="form-group row">
+        <label for="cirugia_cardiaca" class="col-lg-6 control-label">Cirugía cardíaca</label>
+          <label for="cirugia_cardiaca" class="control-label radio-inline col-lg-2">
+            <input type="radio" name="cirugia_cardiaca" value="si"> Sí
+          </label>
+          <label for="cirugia_cardiaca" class="control-label radio-inline col-lg-2" >
+            <input type="radio" name="cirugia_cardiaca" value="no"> No
+          </label>
+          <label for="cirugia_cardiaca" class="control-label radio-inline col-lg-1" >
+            <input type="radio" name="cirugia_cardiaca" value="s_i"> S/I
+          </label>
+      </div>
+
+
+    </div>
+
+    <div class=" col-lg-offset-10 col-lg-2">
+      <button type="submit" class="btn btn-success">Guardar</button>
+    </div>
+    </form>       
+  </div>
+</div>
