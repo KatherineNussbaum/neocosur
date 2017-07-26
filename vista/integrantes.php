76ada72e9ffc1c39f7c786510edd6618d9ecf9e9
KatherineNussbaum <knussbaum@gmail.com>
2017-07-26 00:37:20 -0400

    Avance03

--- vista/integrantes.php
+++ vista/integrantes.php
@@ -7,19 +7,22 @@
     <div class="row">
 
       <div class="col-lg-12">
+
         <div  class="page-header">
           <h1>Neocosur <small>| Integrantes </small></h1>
         </div>
+
         <div>
           <p>
             Colaboradores locales pertenecientes a la red Neocosur
           </p>
-
         </div>
+
         <div class="col-lg-2">
           <h4>Argentina</h4>
         </div>
         <div class="col-lg-10">
+
           <div class="col-lg-4">
             <h5>Clínica y Maternidad Suizo Argentina<br><small>Buenos Aires</small></h5>
           </div>
@@ -56,9 +59,151 @@
             </dl>
           </div>
 
+          <div class="col-lg-4">
+            <h5>Hospital Juan Fernández<br><small>Buenos Aires</small></h5>
+          </div>
+          <div class="col-lg-8 ">
+            <dl class="dl-horizontal">
+              <dt>Jefe Unidad</dt>
+              <dd>Dr. Jorge Tavosnanska</dd>
+            </dl>
+            <dl class="dl-horizontal">
+              <dt>Colaboradores</dt>
+              <dd>Dra. Liliana Roldan<br> Dra. Gladys Saa<br> Dra. Debora Sabatelli<br> Dra. María Laura Gendra<br> Dra. Teresa Sepúlveda<br> Dra. Fernanda Buraschi<br> Dra. Graciela Basso</dd>
+            </dl>
+            <dl class="dl-horizontal">
+              <dt>Seguimiento</dt>
+              <dd>Dr.a Teresa Sepúlveda</dd>
+            </dl>
+          </div>
+
+          <div class="col-lg-4">
+            <h5>Hospital Lagomaggiore<br><small>Mendoza</small></h5>
+          </div>
+          <div class="col-lg-8 ">
+            <dl class="dl-horizontal">
+              <dt>Jefe Unidad</dt>
+              <dd>Dra. Gladys Ferreyra</dd>
+            </dl>
+            <dl class="dl-horizontal">
+              <dt>Colaboradores</dt>
+              <dd>Dra. Gladys Ferreyra</dd>
+            </dl>
+            <dl class="dl-horizontal">
+              <dt>Seguimiento</dt>
+              <dd>Dra. María Otilia Ranzuglia<br> Dra. Mariana Brusadin<br> Dra. Giannina Chiaramonte<br> Dra. Carolina Atencio<br> Dra. Rocío Fontana. </dd>
+            </dl>
+          </div>
+
+          <div class="col-lg-4">
+            <h5>Maternidad Sardá<br><small>Buenos Aires</small></h5>
+          </div>
+          <div class="col-lg-8 ">
+            <dl class="dl-horizontal">
+              <dt>Jefe Unidad</dt>
+              <dd>Dr.Claudio Solana</dd>
+            </dl>
+            <dl class="dl-horizontal">
+              <dt>Colaboradores</dt>
+              <dd>Dr. Claudio Solana<br> Dr. Elio Rojas<br> Dr. Javier Meritano<br> Dr. Ricardo Nieto</dd>
+            </dl>
+            <dl class="dl-horizontal">
+              <dt>Seguimiento</dt>
+              <dd>Dra. Mónica Brundi<br> Dra. Laura Kasten<br> Dra. Roxana Borroni<br> Dra. Silvana Vivas<br> Dra. Alejandra Fiorentino</dd>
+            </dl>
+          </div>
+
+          <div class="col-lg-4">
+            <h5>Sanatorio de la Trinidad<br><small>Buenos Aires</small></h5>
+          </div>
+          <div class="col-lg-8 ">
+            <dl class="dl-horizontal">
+              <dt>Jefe Unidad</dt>
+              <dd>Dr. Nestor Vain</dd>
+            </dl>
+            <dl class="dl-horizontal">
+              <dt>Colaboradores</dt>
+              <dd>Dra. Edith Martínez<br> Dra. Cecilia Garcia<br> Dr. Néstos Vain</dd>
+            </dl>
+            <dl class="dl-horizontal">
+              <dt>Seguimiento</dt>
+              <dd>Dra. Fabiana Herbon</dd>
+            </dl>
+          </div>
+
+          <div class="col-lg-4">
+            <h5>Hospital Español de Mendoza<br><small>Mendoza</small></h5>
+          </div>
+          <div class="col-lg-8 ">
+            <dl class="dl-horizontal">
+              <dt>Jefe Unidad</dt>
+              <dd>Horacio Roge</dd>
+            </dl>
+            <dl class="dl-horizontal">
+              <dt>Colaboradores</dt>
+              <dd>Dr. Daniel Abdala<br> Dr. Martín Guida<br> Dr. Ignacio Roge<br> Dr. Damián Pretz<br> Dra. Lorena Gamboa<br> Dra. Marcela Otarola</dd>
+            </dl>  
+          </div>
+
+          <div class="col-lg-4">
+            <h5>Hospital Universitario Austral<br><small></small></h5>
+          </div>
+          <div class="col-lg-8 ">
+            <dl class="dl-horizontal">
+              <dt>Jefe Unidad</dt>
+              <dd>Dr. Gabriel Musante</dd>
+            </dl>
+            <dl class="dl-horizontal">
+              <dt>Colaboradores</dt>
+              <dd>Dr. Rodolfo Keller<br> Dra. Magdalena De Elizalde<br> Dr. Juan Pablo Berazategui<br> Dra.Leila Acha<br> Dr. Gabriel Musante</dd>
+            </dl>
+            <dl class="dl-horizontal">
+              <dt>Seguimiento</dt>
+              <dd>Dra. Leila Acha<br> Dra. Carola Capelli<br> Dra. Magdalena De Elizalde<br>Dr. Rodolfo Keller</dd>
+            </dl>
+          </div>
+
+          <div class="col-lg-4">
+            <h5>Hospital Nuestra Señora de la Misericordia<br><small></small></h5>
+          </div>
+          <div class="col-lg-8 ">
+            <dl class="dl-horizontal">
+              <dt>Jefe Unidad</dt>
+              <dd>Dr. Luis Ahumada</dd>
+            </dl>
+            <dl class="dl-horizontal">
+              <dt>Colaboradores</dt>
+              <dd>Dr. Luis Ahumada<br> Dra. Mirta Ferreyra<br> Dra. Laura Sánchez<br> Dra. Adriana Mitrano</dd>
+            </dl>
+          </div>
+
+          <div class="col-lg-4">
+            <h5>Clínica Universitaria Reina Fabiola<br><small></small></h5>
+          </div>
+          <div class="col-lg-8 ">
+            <dl class="dl-horizontal">
+              <dt>Jefe Unidad</dt>
+              <dd>Dr. Ignacio Sosa</dd>
+            </dl>
+            <dl class="dl-horizontal">
+              <dt>Colaboradores</dt>
+              <dd>Dr. Carlos Maure</dd>
+            </dl>
+            <dl class="dl-horizontal">
+              <dt>Seguimiento</dt>
+              <dd>Dra. María Van Ooteghem<br> Dr. Igancio Sosa<br> Dr. Carlos Maure<br> Dra. Deborah Wainstein</dd>
+            </dl>
+          </div>
+
+
+
+
+
         </div>
+
       </div>
     </div>
+</div>
 
     <!-- Fin del Contenido -->  
 </div>    
---
+++ vista/integrantes2.php
@@ -0,0 +1,411 @@
+<?php include 'head.php'; ?>
+
+<div class="container">
+  <!-- Inicio del Contenido -->
+    <?php include 'header.php'; ?>
+    <!-- Inicio de Título -->
+    <div class="row">
+
+      <div class="col-lg-12">
+
+        <div  class="page-header">
+          <h1>Neocosur <small>| Integrantes </small></h1>
+        </div>
+
+        <div>
+          <p>
+            Colaboradores locales pertenecientes a la red Neocosur
+          </p>
+        </div>
+        <div class="col-lg-2">
+          <ul class="nav nav-pills nav-stacked">
+            <li role="presentation" class="active"><a href="#">Argentina</a></li>
+            <li role="presentation"><a href="#">Brasil</a></li>
+            <li role="presentation"><a href="#">Chile</a></li>        
+          </ul>
+        </div>
+        <div class="col-lg-10">
+          <div id="argentina">
+
+            <div id="accordion_argentina">
+
+              <h5 >Clínica y Maternidad Suizo Argentina<br><small>Buenos Aires</small></h5>
+              <div>
+                <dl class="dl-horizontal">
+                  <dt>Jefe Unidad</dt>
+                  <dd>Dr. Luis Prudent<br> Ana Pedraza.</dd>
+                </dl>
+                <dl class="dl-horizontal">
+                  <dt>Colaboradores</dt>
+                  <dd>Dr. Guillermo Colantonio<br> Dr. Jorge Zapata<br> Dr. Gastón Pérez<br> Dr. Tomás Fortunato</dd>
+                </dl>
+                <dl class="dl-horizontal">
+                  <dt>Seguimiento</dt>
+                  <dd>Dra. Nélida Aguilera<br> Dra. Susana García<br>Dra. Alcira Escandar</dd>
+                </dl>
+              </div>
+
+              <h5>Hospital Italiano<br><small>Buenos Aires</small></h5>
+              <div>
+                <dl class="dl-horizontal">
+                  <dt>Jefe Unidad</dt>
+                  <dd>Dr. Gónzalo Mariani</dd>
+                </dl>
+                <dl class="dl-horizontal">
+                  <dt>Colaboradores</dt>
+                  <dd>Dr. Gónzalo Mariani<br>Dra. Silvia Fernández <br>Dra. María Fernanda Galleti<br>Dr. Pablo Brener</dd>
+                </dl>
+                <dl class="dl-horizontal">
+                  <dt>Seguimiento</dt>
+                  <dd>Dra. Diana Rodríguez<br> Dra. María Edwards<br> Dra.Cecilia Martinez<br> Dra.Laura Guzzetti</dd>
+                </dl>
+              </div>
+
+
+              <h5>Hospital Juan Fernández<br><small>Buenos Aires</small></h5>
+              <div>
+                <dl class="dl-horizontal">
+                  <dt>Jefe Unidad</dt>
+                  <dd>Dr. Jorge Tavosnanska</dd>
+                </dl>
+                <dl class="dl-horizontal">
+                  <dt>Colaboradores</dt>
+                  <dd>Dra. Liliana Roldan<br> Dra. Gladys Saa<br> Dra. Debora Sabatelli<br> Dra. María Laura Gendra<br> Dra. Teresa Sepúlveda<br> Dra. Fernanda Buraschi<br> Dra. Graciela Basso</dd>
+                </dl>
+                <dl class="dl-horizontal">
+                  <dt>Seguimiento</dt>
+                  <dd>Dr.a Teresa Sepúlveda</dd>
+                </dl>
+              </div>
+
+
+              <h5>Hospital Lagomaggiore<br><small>Mendoza</small></h5>
+
+              <div>
+                <dl class="dl-horizontal">
+                  <dt>Jefe Unidad</dt>
+                  <dd>Dra. Gladys Ferreyra</dd>
+                </dl>
+                <dl class="dl-horizontal">
+                  <dt>Colaboradores</dt>
+                  <dd>Dra. Gladys Ferreyra</dd>
+                </dl>
+                <dl class="dl-horizontal">
+                  <dt>Seguimiento</dt>
+                  <dd>Dra. María Otilia Ranzuglia<br> Dra. Mariana Brusadin<br> Dra. Giannina Chiaramonte<br> Dra. Carolina Atencio<br> Dra. Rocío Fontana. </dd>
+                </dl>
+              </div>
+
+              <h5>Maternidad Sardá<br><small>Buenos Aires</small></h5>
+              <div>
+                <dl class="dl-horizontal">
+                  <dt>Jefe Unidad</dt>
+                  <dd>Dr.Claudio Solana</dd>
+                </dl>
+                <dl class="dl-horizontal">
+                  <dt>Colaboradores</dt>
+                  <dd>Dr. Claudio Solana<br> Dr. Elio Rojas<br> Dr. Javier Meritano<br> Dr. Ricardo Nieto</dd>
+                </dl>
+                <dl class="dl-horizontal">
+                  <dt>Seguimiento</dt>
+                  <dd>Dra. Mónica Brundi<br> Dra. Laura Kasten<br> Dra. Roxana Borroni<br> Dra. Silvana Vivas<br> Dra. Alejandra Fiorentino</dd>
+                </dl>
+              </div>
+
+              <h5>Sanatorio de la Trinidad<br><small>Buenos Aires</small></h5>
+              <div>
+                <dl class="dl-horizontal">
+                  <dt>Jefe Unidad</dt>
+                  <dd>Dr. Nestor Vain</dd>
+                </dl>
+                <dl class="dl-horizontal">
+                  <dt>Colaboradores</dt>
+                  <dd>Dra. Edith Martínez<br> Dra. Cecilia Garcia<br> Dr. Néstos Vain</dd>
+                </dl>
+                <dl class="dl-horizontal">
+                  <dt>Seguimiento</dt>
+                  <dd>Dra. Fabiana Herbon</dd>
+                </dl>
+              </div>
+
+              <h5>Hospital Español de Mendoza<br><small>Mendoza</small></h5>
+              <div>
+                <dl class="dl-horizontal">
+                  <dt>Jefe Unidad</dt>
+                  <dd>Horacio Roge</dd>
+                </dl>
+                <dl class="dl-horizontal">
+                  <dt>Colaboradores</dt>
+                  <dd>Dr. Daniel Abdala<br> Dr. Martín Guida<br> Dr. Ignacio Roge<br> Dr. Damián Pretz<br> Dra. Lorena Gamboa<br> Dra. Marcela Otarola</dd>
+                </dl>  
+              </div>
+
+              <h5>Hospital Universitario Austral<br><small></small></h5>
+              <div>
+                <dl class="dl-horizontal">
+                  <dt>Jefe Unidad</dt>
+                  <dd>Dr. Gabriel Musante</dd>
+                </dl>
+                <dl class="dl-horizontal">
+                  <dt>Colaboradores</dt>
+                  <dd>Dr. Rodolfo Keller<br> Dra. Magdalena De Elizalde<br> Dr. Juan Pablo Berazategui<br> Dra.Leila Acha<br> Dr. Gabriel Musante</dd>
+                </dl>
+                <dl class="dl-horizontal">
+                  <dt>Seguimiento</dt>
+                  <dd>Dra. Leila Acha<br> Dra. Carola Capelli<br> Dra. Magdalena De Elizalde<br>Dr. Rodolfo Keller</dd>
+                </dl>
+              </div>
+
+              <h5>Hospital Nuestra Señora de la Misericordia<br><small></small></h5>
+              <div>
+                <dl class="dl-horizontal">
+                  <dt>Jefe Unidad</dt>
+                  <dd>Dr. Luis Ahumada</dd>
+                </dl>
+                <dl class="dl-horizontal">
+                  <dt>Colaboradores</dt>
+                  <dd>Dr. Luis Ahumada<br> Dra. Mirta Ferreyra<br> Dra. Laura Sánchez<br> Dra. Adriana Mitrano</dd>
+                </dl>
+              </div>
+
+              <h5>Clínica Universitaria Reina Fabiola<br><small></small></h5>
+              <div>
+                <dl class="dl-horizontal">
+                  <dt>Jefe Unidad</dt>
+                  <dd>Dr. Ignacio Sosa</dd>
+                </dl>
+                <dl class="dl-horizontal">
+                  <dt>Colaboradores</dt>
+                  <dd>Dr. Carlos Maure</dd>
+                </dl>
+                <dl class="dl-horizontal">
+                  <dt>Seguimiento</dt>
+                  <dd>Dra. María Van Ooteghem<br> Dr. Igancio Sosa<br> Dr. Carlos Maure<br> Dra. Deborah Wainstein</dd>
+                </dl>
+              </div>
+
+            </div>
+          </div>
+          
+          <div id="brasil">
+
+            <div id="accordion_brasil">
+
+              <h5 class="txt_destacado grey" >Hospital Universitario UFMA<br><small></small></h5>
+              <div>
+                <dl class="dl-horizontal">
+                  <dt>Jefe Unidad</dt>
+                  <dd>Dra. Marynea Do Vale</dd>
+                </dl>
+                <dl class="dl-horizontal">
+                  <dt>Colaboradores</dt>
+                  <dd> Dra. Vanda María Ferriera<br> Dra. María José Silva<br> Dra. Marynea Do Vale</dd>
+                </dl>
+                <dl class="dl-horizontal">
+                  <dt>Seguimiento</dt>
+                  <dd>Dra. Nélida Aguilera<br> Dra. Susana García<br>Dra. Alcira Escandar</dd>
+                </dl>
+              </div>
+            </div>
+
+          </div>
+
+
+          <div id="chile">
+
+            <div id="accordion_chile">
+
+              <h5 class="txt_destacado grey" >Hospital Clínico Universidad Católica de Chile<br><small>Santiago</small></h5>
+              <div>
+                <dl class="dl-horizontal">
+                  <dt>Jefe Unidad</dt>
+                  <dd>Dra. Paulina Toso</dd>
+                </dl>
+                <dl class="dl-horizontal">
+                  <dt>Colaboradores</dt>
+                  <dd>Dra. Daniela Masoli<br> Dr. Alberto Estay<br> Jorge Fabres</dd>
+                </dl>
+                <dl class="dl-horizontal">
+                  <dt>Seguimiento</dt>
+                  <dd>No</dd>
+                </dl>
+              </div>
+
+              <h5 class="txt_destacado grey" >Hospital Clínico Universidad de Chile<br><small>Santiago</small></h5>
+              <div>
+                <dl class="dl-horizontal">
+                  <dt>Jefe Unidad</dt>
+                  <dd>Dr. Luis Hernán González</dd>
+                </dl>
+                <dl class="dl-horizontal">
+                  <dt>Colaboradores</dt>
+                  <dd> Dr. Jaime Burgos<br> Dr. Rodrigo Ramírez<br> Dra. M. Eugenia Hübner</dd>
+                </dl>
+                <dl class="dl-horizontal">
+                  <dt>Seguimiento</dt>
+                  <dd>No Realiza</dd>
+                </dl>
+              </div>
+
+              <h5 class="txt_destacado grey" >Hospital Guillermo Grant<br><small>Concepción</small></h5>
+              <div>
+                <dl class="dl-horizontal">
+                  <dt>Jefe Unidad</dt>
+                  <dd>Dr. Jorge León del Pedregal</dd>
+                </dl>
+                <dl class="dl-horizontal">
+                  <dt>Colaboradores</dt>
+                  <dd>Dra. Lilia Campos<br> Dr. Jorge Leon del Pedregal<br> Dra. Jimena Giaconi<br> Dr. Horacio Contreras<br> Dra. Lilian Cifuentes</dd>
+                </dl>
+              </div>
+
+              <h5 class="txt_destacado grey" >Hospital Guillermo Grant<br><small>Viña del Mar</small></h5>
+              <div>
+                <dl class="dl-horizontal">
+                  <dt>Jefe Unidad</dt>
+                  <dd>Dra. Alejandra Nuñez</dd>
+                </dl>
+                <dl class="dl-horizontal">
+                  <dt>Colaboradores</dt>
+                  <dd>Dra. Daniela Sandino<br> Dr. Galo Bajaña<br> Dra. Priscilla Durán</dd>
+                </dl>
+              </div>
+
+              <h5 class="txt_destacado grey" >Hospital San José<br><small>Santiago</small></h5>
+              <div>
+                <dl class="dl-horizontal">
+                  <dt>Jefe Unidad</dt>
+                  <dd>Dra. Agustina González</dd>
+                </dl>
+                <dl class="dl-horizontal">
+                  <dt>Colaboradores</dt>
+                  <dd>Dra. Claudia Ávila<br> Dra. Lorena Tapia<br> Dra. Agustina González</dd>
+                </dl>
+                <dl class="dl-horizontal">
+                  <dt>Seguimiento</dt>
+                  <dd>Dra. Mónica Morgues<br> Dra. Patricia Vernal<br> Dra. Scarlett Bretahuer</dd>
+                </dl>
+              </div>
+
+              <h5 class="txt_destacado grey" >Hospital Dr. Sótero del Río<br><small>Santiago</small></h5>
+              <div>
+                <dl class="dl-horizontal">
+                  <dt>Jefe Unidad</dt>
+                  <dd>Dr. Juan Pablo Cortés</dd>
+                </dl>
+                <dl class="dl-horizontal">
+                  <dt>Colaboradores</dt>
+                  <dd>Dra. Patricia Mena<br> Claudia Toro<br> M. José Escalante<br> Marcela Díaz<br> Dra. Romy Olate</dd>
+                </dl>
+                <dl class="dl-horizontal">
+                  <dt>Seguimiento</dt>
+                  <dd>Dra. Ivonne D´Apremont<br> Dra. Enrica Pittaluga</dd>
+                </dl>
+              </div>
+
+              <h5 class="txt_destacado grey" >Hospital San Borja Arriarán<br><small>Santiago</small></h5>
+              <div>
+                <dl class="dl-horizontal">
+                  <dt>Jefe Unidad</dt>
+                  <dd>Dra. Mirna García</dd>
+                </dl>
+                <dl class="dl-horizontal">
+                  <dt>Colaboradores</dt>
+                  <dd>Dr. R.aúl Vélez<br> Dra. María Caballero</dd>
+                </dl>
+                <dl class="dl-horizontal">
+                  <dt>Seguimiento</dt>
+                  <dd>Dr. Rafael Mendizabal<br> Dra. Katherine Rossel</dd>
+                </dl>
+              </div>
+
+              <h5 class="txt_destacado grey" >Hospital de Puerto Montt<br><small></small></h5>
+              <div>
+                <dl class="dl-horizontal">
+                  <dt>Jefe Unidad</dt>
+                  <dd>Dr. Rodrigo Donoso</dd>
+                </dl>
+                <dl class="dl-horizontal">
+                  <dt>Colaboradores</dt>
+                  <dd>Dr. Gerardo Flores<br> Dra. Patricia Alvarez<br> Dr. Mauricio Marín<br> Dr. Manuel Paredes<br> Dra. Johanne Jahnsen<br> Dra. Virginia Alpaca<br> Dra. María Eugenia Aguierre<br> Dr. Jaime García<br> Dra. Ivian Blanco</dd>
+                </dl>
+                <dl class="dl-horizontal">
+                  <dt>Seguimiento</dt>
+                  <dd>Dr. Rafael Mendizabal<br> Dra. Katherine Rossel</dd>
+                </dl>
+              </div>
+
+              <h5 class="txt_destacado grey" >Hospital Van Buren<br><small></small></h5>
+              <div>
+                <dl class="dl-horizontal">
+                  <dt>Jefe Unidad</dt>
+                  <dd>Dr. Miguel Aravena</dd>
+                </dl>
+                <dl class="dl-horizontal">
+                  <dt>Colaboradores</dt>
+                  <dd>Dr. Eduardo Ahumada<br> Dra. Paula Gajardo<br> Dra. Montserrat Vásquez</dd>
+                </dl>
+                <dl class="dl-horizontal">
+                  <dt>Seguimiento</dt>
+                  <dd>Dra. Cecilia Guerra<br> Dr. Eduardo Ahumada<br> Dra. Monserrat Vasquez</dd>
+                </dl>
+              </div>
+
+              <h5 class="txt_destacado grey" >Hospital Regional de Antofagasta<br><small></small></h5>
+              <div>
+                <dl class="dl-horizontal">
+                  <dt>Jefe Unidad</dt>
+                  <dd>Dra. Ximena Sandoval</dd>
+                </dl>
+                <dl class="dl-horizontal">
+                  <dt>Colaboradores</dt>
+                  <dd>Dra. Ximena Sandoval<br> Dr. Sergio Farías<br> Dra. Jocelyn Tapia</dd>
+                </dl>
+              </div>
+
+            </div>
+            
+          </div>
+         
+
+
+
+
+
+        </div>
+
+      </div>
+    </div>
+</div>
+
+    <!-- Fin del Contenido -->  
+</div>    
+
+<?php
+  include 'footer.php';
+?>     
+<!-- Inicio de JavaScript -->
+<script src="../js/jquery.js"></script>
+<script src="../js/jquery-ui.js"></script>
+<script src="../js/bootstrap.js"></script>
+<script src="../js/npm.js"></script>
+<script src="../js/neocosur.js"></script>
+<script src="../js/data_table.min.js"></script>
+<script src="../js/data_table_button.js"></script>
+<script src="../js/jjszip.min.js"></script>
+<script src="../js/buttons.html5.min.js"></script>
+<script src="../js/data_table_print.js"></script>
+<script type="text/javascript">
+  $(document).ready(function() {
+      $('#tbl_comite').DataTable();
+
+      $( "#accordion_argentina" ).accordion();
+      $( "#accordion_brasil" ).accordion();
+      $( "#accordion_chile" ).accordion();
+
+  } );
+</script>
+
+</body>
+</html>
+
