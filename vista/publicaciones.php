5290df451888bb0de8b091610ba85fecc96c96e4
Katherine Nussbaum <knussbaum@gmail.com>
2017-10-15 12:42:07 -0300

    Diseño2017-10-15

--- vista/publicaciones.php
+++ vista/publicaciones.php
@@ -8,63 +8,99 @@
       <div class="row">
         <h1 class="display-4">Publicaciones Neocosur</h1>
       </div>
-      <div class="row">
-        <div class="col-4 publicacion">
-            <div class="card">
-              <div class="card-body">
-                <h5 class="card-title">Effect of prophylactic CPAP in very low birth weight infants in South America 2016</h5>
-                  <p class="card-text">JR Zubizarreta  1,2,10, SA Lorch 3,4,5,6,10 , G Marshall 7,8,ID ’ Apremont 9 and JL Tapia<br>
-                  for the South American Neocosur Network</p>
-                  <button type="button" class="btn btn-primary">Adjuntos</button> 
-                  <button type="button" class="btn btn-success">Ver</button>
-              </div>
-            </div>
-          </div>
 
-          <div class="col-4 publicacion" >
-            <div class="card">
-              <div class="card-body">
-                <h5 class="card-title">Mode of delivery and antenatal steroids and their association with survival and severe intraventricular hemorrhage in very low birth weight infants 2016</h5>
-                  <p class="card-text">ME Hübner, R Ramirez, J Burgos, A Dominguez, JL Tapia and the Neocosur Neonatal Network<</p>
-                  <button type="button" class="btn btn-primary">Adjuntos</button> 
-                  <button type="button" class="btn btn-success">Ver</button>
-              </div>
-            </div>
-          </div>
+      <div class="row noticia">
+        <div class="col-lg-4 img">
+          <img src="../img/revistas/2016_01.jpg" >
+        </div>
+        <div class="col-lg-8">
+          <h5 class="card-title">Effect of prophylactic CPAP in very low birth weight infants in South America 2016</h5>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+          cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+          proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+          <button type="button" class="btn btn-default">Adjuntos</button> 
+          <button type="button" class="btn btn-default">Ver</button>
+        </div>
+      </div>
 
-          <div class="col-4 publicacion">
-            <div class="card">
-              <div class="card-body">
-                <h5 class="card-title">Impact of maternal diabetes mellitus on mortality and morbidity of very low birth weight infants: a multicenter Latin America study</h5>
-                  <p class="card-text">2015, Carlos Grandi, Jose L. Tapia, Viviane C. Cardosok</p>
-                  <button type="button" class="btn btn-primary">Adjuntos</button> 
-                  <button type="button" class="btn btn-success">Ver</button>
-              </div>
-            </div>
-          </div>
+      <div class="row noticia">
+        <div class="col-lg-4 img">
+          <img src="../img/revistas/2016_01.jpg" >
+        </div>
+        <div class="col-lg-8">
+          <h5 class="card-title">Effect of prophylactic CPAP in very low birth weight infants in South America 2016</h5>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+          cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+          proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </p>
 
-          <div class="col-4 publicacion">
-            <div class="card">
-              <div class="card-body">
-                <h5 class="card-title">Estrés en padres de recién nacidos prematuros de muy bajo peso hospitalizados en unidades de cuidados intensivos neonatales. Estudio multicéntrico</h5>
-                  <p class="card-text">2015, Lic. Francisca Wormald, Dr. José L. Tapia, Dra. Gabriela Torres, Lic. Paula Cánepa, Lic. María Aurelia González, Dra. Diana Rodríguez, Dra. Marisol Escobar, Lic. Bernardita Reyes, Dra. Carola Capelli, Lic. Laura Menéndez, Dra. Patricia Delgado, Dr. Sergio Treuer, Dr. Rodrigo Ramírez, Lic. Norma Borja, M. Sc. Angélica Domínguez y la Red Neonatal Neocosur</p>
-                  <button type="button" class="btn btn-primary">Adjuntos</button> 
-                  <button type="button" class="btn btn-success">Ver</button>
-              </div>
-            </div>
-          </div>
+          <button type="button" class="btn btn-default">Adjuntos</button> 
+          <button type="button" class="btn btn-default">Ver</button>
+        </div>
+      </div>
 
-          <div class="col-4 publicacion">
-            <div class="card">
-              <div class="card-body">
-                <h5 class="card-title">Evacuación del Meconio Intestinal Para Mejorar Tolerancia Alimentaria En Prematuro De Muy Bajo Peso” (Protocolo Emita)</h5>
-                  <p class="card-text">2014, Patricia Mena; Jorge León del Pedregal; Daniela Sandino; Pamela Ralmolfo B.; Débora Sabatelli; Adolfo Llanos M.; Beatriz Milet L.; RED Neonatal NEOCOSUR.<br>
-                  Rev Chil Pediatr 2014;85(3):304-311</p>
-                  <button type="button" class="btn btn-primary">Adjuntos</button> 
-                  <button type="button" class="btn btn-success">Ver</button>
-              </div>
-            </div>
-          </div>
+      <div class="row noticia">
+        <div class="col-lg-4 img">
+          <img src="../img/revistas/2016_01.jpg" >
+        </div>
+        <div class="col-lg-8">
+          <h5 class="card-title">Effect of prophylactic CPAP in very low birth weight infants in South America 2016</h5>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+          cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+          proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+
+          <button type="button" class="btn btn-default">Adjuntos</button> 
+          <button type="button" class="btn btn-default">Ver</button>
+        </div>
+      </div>
+
+      <div class="row noticia">
+        <div class="col-lg-4 img">
+          <img src="../img/revistas/2016_01.jpg" >
+        </div>
+        <div class="col-lg-8">
+          <h5 class="card-title">Effect of prophylactic CPAP in very low birth weight infants in South America 2016</h5>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+          cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+          proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+
+          <button type="button" class="btn btn-default">Adjuntos</button> 
+          <button type="button" class="btn btn-default">Ver</button>
+        </div>
+      </div>
+
+      <div class="row noticia">
+        <div class="col-lg-4 img">
+          <img src="../img/revistas/2016_01.jpg" >
+        </div>
+        <div class="col-lg-8">
+          <h5 class="card-title">Effect of prophylactic CPAP in very low birth weight infants in South America 2016</h5>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+          cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+          proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+
+          <button type="button" class="btn btn-default">Adjuntos</button> 
+          <button type="button" class="btn btn-default">Ver</button>
+        </div>
       </div>
     </div>
 </div>
--- vista/revistas.php
+++ vista/revistas.php
@@ -7,98 +7,100 @@
       <div class="row">
         <h1 class="display-4">Revista Neocosur</h1>
       </div>
-      <div class="row revista">
-          <div class="col">
-            <div class="card">
-              <img class="card-img-top" src="../img/revistas/2016_01.jpg">
-              <div class="card-body">
-                <h3 class="card-title">2013, N°5</h3>
-                <h6 class="card-text">Aspectos del cuidado del prematuro tardío después del alta hospitalaria</h6>
-                <p class="text-right"><a href="" class="btn btn-light" role="button">Descargar</a></p>
-              </div>
-            </div>
-          </div>
 
-          <div class="col">
-            <div class="card">
-              <img class="card-img-top" src="../img/revistas/2016_01.jpg">
-              <div class="card-body">
-                <h3 class="card-title">2013, N°5</h3>
-                <h6 class="card-text">Aspectos del cuidado del prematuro tardío después del alta hospitalaria</h6>
-                <p class="text-right"><a href="" class="btn btn-light" role="button">Descargar</a></p>
-              </div>
-            </div>
-          </div>
-
-
-          <div class="col">
-            <div class="card">
-              <img class="card-img-top" src="../img/revistas/2016_01.jpg">
-              <div class="card-body">
-                <h3 class="card-title">2013, N°5</h3>
-                <h6 class="card-text">Aspectos del cuidado del prematuro tardío después del alta hospitalaria</h6>
-                <p class="text-right"><a href="" class="btn btn-light" role="button">Descargar</a></p>
-              </div>
-            </div>
-          </div>
-
-          <div class="col">
-            <div class="card">
-              <img class="card-img-top" src="../img/revistas/2016_01.jpg">
-              <div class="card-body">
-                <h3 class="card-title">2013, N°5</h3>
-                <h6 class="card-text">Aspectos del cuidado del prematuro tardío después del alta hospitalaria</h6>
-                <p class="text-right"><a href="" class="btn btn-light" role="button">Descargar</a></p>
-              </div>
-            </div>
-          </div>
+      <div class="row noticia">
+        <div class="col-lg-4 img">
+          <img src="../img/revistas/2016_01.jpg" >
+        </div>
+        <div class="col-lg-8">
+          <h5 class="card-title">Effect of prophylactic CPAP in very low birth weight infants in South America 2016</h5>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+          cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+          proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+          <p class="card-text">JR Zubizarreta  1,2,10, SA Lorch 3,4,5,6,10 , G Marshall 7,8,ID ’ Apremont 9 and JL Tapia<br>
+          for the South American Neocosur Network</p>
+          <button type="button" class="btn btn-default">Descargar</button> 
+        </div>
       </div>
 
-      <div class="row revista">
-          <div class="col">
-            <div class="card">
-              <img class="card-img-top" src="../img/revistas/2016_01.jpg">
-              <div class="card-body">
-                <h3 class="card-title">2013, N°5</h3>
-                <h6 class="card-text">Aspectos del cuidado del prematuro tardío después del alta hospitalaria</h6>
-                <p class="text-right"><a href="" class="btn btn-light" role="button">Descargar</a></p>
-              </div>
-            </div>
-          </div>
+      <div class="row noticia">
+        <div class="col-lg-4 img">
+          <img src="../img/revistas/2016_01.jpg" >
+        </div>
+        <div class="col-lg-8">
+          <h5 class="card-title">Effect of prophylactic CPAP in very low birth weight infants in South America 2016</h5>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+          cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+          proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+          <p class="card-text">JR Zubizarreta  1,2,10, SA Lorch 3,4,5,6,10 , G Marshall 7,8,ID ’ Apremont 9 and JL Tapia<br>
+          for the South American Neocosur Network</p>
+          <button type="button" class="btn btn-default">Descargar</button> 
+        </div>
+      </div>
 
-          <div class="col">
-            <div class="card">
-              <img class="card-img-top" src="../img/revistas/2016_01.jpg">
-              <div class="card-body">
-                <h3 class="card-title">2013, N°5</h3>
-                <h6 class="card-text">Aspectos del cuidado del prematuro tardío después del alta hospitalaria</h6>
-                <p class="text-right"><a href="" class="btn btn-light" role="button">Descargar</a></p>
-              </div>
-            </div>
-          </div>
+      <div class="row noticia">
+        <div class="col-lg-4 img">
+          <img src="../img/revistas/2016_01.jpg" >
+        </div>
+        <div class="col-lg-8">
+          <h5 class="card-title">Effect of prophylactic CPAP in very low birth weight infants in South America 2016</h5>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+          cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+          proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+          <p class="card-text">JR Zubizarreta  1,2,10, SA Lorch 3,4,5,6,10 , G Marshall 7,8,ID ’ Apremont 9 and JL Tapia<br>
+          for the South American Neocosur Network</p>
+          <button type="button" class="btn btn-default">Descargar</button> 
+        </div>
+      </div>
 
+      <div class="row noticia">
+        <div class="col-lg-4 img">
+          <img src="../img/revistas/2016_01.jpg" >
+        </div>
+        <div class="col-lg-8">
+          <h5 class="card-title">Effect of prophylactic CPAP in very low birth weight infants in South America 2016</h5>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+          cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+          proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+          <p class="card-text">JR Zubizarreta  1,2,10, SA Lorch 3,4,5,6,10 , G Marshall 7,8,ID ’ Apremont 9 and JL Tapia<br>
+          for the South American Neocosur Network</p>
+          <button type="button" class="btn btn-default">Descargar</button> 
+        </div>
+      </div>
 
-          <div class="col">
-            <div class="card">
-              <img class="card-img-top" src="../img/revistas/2016_01.jpg">
-              <div class="card-body">
-                <h3 class="card-title">2013, N°5</h3>
-                <h6 class="card-text">Aspectos del cuidado del prematuro tardío después del alta hospitalaria</h6>
-                <p class="text-right"><a href="" class="btn btn-light" role="button">Descargar</a></p>
-              </div>
-            </div>
-          </div>
-
-          <div class="col">
-            <div class="card">
-              <img class="card-img-top" src="../img/revistas/2016_01.jpg">
-              <div class="card-body">
-                <h3 class="card-title">2013, N°5</h3>
-                <h6 class="card-text">Aspectos del cuidado del prematuro tardío después del alta hospitalaria</h6>
-                <p class="text-right"><a href="" class="btn btn-light" role="button">Descargar</a></p>
-              </div>
-            </div>
-          </div>
+      <div class="row noticia">
+        <div class="col-lg-4 img">
+          <img src="../img/revistas/2016_01.jpg" >
+        </div>
+        <div class="col-lg-8">
+          <h5 class="card-title">Effect of prophylactic CPAP in very low birth weight infants in South America 2016</h5>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+          cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+          proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+          <p class="card-text">JR Zubizarreta  1,2,10, SA Lorch 3,4,5,6,10 , G Marshall 7,8,ID ’ Apremont 9 and JL Tapia<br>
+          for the South American Neocosur Network</p>
+          <button type="button" class="btn btn-default">Descargar</button> 
+        </div>
       </div>
     </div>
 
